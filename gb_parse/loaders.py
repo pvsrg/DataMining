71458c125f5c68ef86892d3ab53a89555d2607f7
pvsrg <pvsrg68@rambler.ru>
2021-04-16 23:05:45 +0300

    Homework5

---
+++ gb_parse/loaders.py
@@ -0,0 +1,45 @@
+from urllib.parse import urljoin
+from scrapy.loader import ItemLoader
+from itemloaders.processors import MapCompose, TakeFirst
+
+
+def flat_text(items):
+    return '\n'.join(items)
+
+
+def join_text(items):
+    return ' '.join(items)
+
+
+def split_text(items):
+    return items.split(',')
+
+
+def lower_text(items):
+    return items.lower()
+
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
+
+class HHLoader(ItemLoader):
+    default_item_class = dict
+    collection_out = TakeFirst()
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    salary_out = join_text
+    description_out = flat_text
+    author_in = MapCompose(hh_user_url)
+    author_out = TakeFirst()
+
+
+class HCLoader(ItemLoader):
+    default_item_class = dict
+    collection_out = TakeFirst()
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    site_out = TakeFirst()
+    activities_in = MapCompose(lower_text)
+    activities_out = MapCompose(split_text)
+    description_out = TakeFirst()
---
+++ gb_parse/spiders/xpaths.py
@@ -0,0 +1,28 @@
+HH_PAGE_XPATH = {
+    "vacancy": '//div[@data-qa="vacancy-serp__results"]'
+               '//a[@data-qa="vacancy-serp__vacancy-title"]/@href',
+    "company": '//div[@data-qa="vacancy-serp__results"]'
+               '//a[@data-qa="vacancy-serp__vacancy-employer"]/@href',
+    "pagination": '//div[@data-qa="pager-block"]//'
+                  'a[@class="bloko-button"]/@href',
+}
+
+HH_VACANCY_XPATH = {
+    "title": '//h1[@data-qa="vacancy-title"]/text()',
+    "salary": '//p[@class="vacancy-salary"]/span/text()',
+    "description": '//div[@data-qa="vacancy-description"]//text()',
+    "skills": '//div[@class="bloko-tag-list"]//'
+              'div[contains(@data-qa, "skills-element")]/'
+              'span[@data-qa="bloko-tag__text"]/text()',
+    "author": '//a[@data-qa="vacancy-company-name"]/@href',
+}
+
+HH_COMPANY_XPATH = {
+    "title": '//div[@class="company-header"]//'
+             'span[@data-qa="company-header-title-name"]/text()',
+    "site": '//div[@class="employer-sidebar"]//'
+            'a[@data-qa="sidebar-company-site"]/@href',
+    "activities": '//div[@class="employer-sidebar-content"]//'
+                  'div[@class="employer-sidebar-block"]/p/text()',
+    "description": '//div[@data-qa="company-description-text"]//text()',
+}
